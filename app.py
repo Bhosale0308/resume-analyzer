df2960fe538835edbc96a42add4684b39626b862
Bhosale0308 <bhosalesneha382005@gmail.com>
2026-04-08 20:10:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -195,4 +195,6 @@ def create_tables():
 # with app.app_context():
 #     db.create_all()
 if __name__ == '__main__':
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
